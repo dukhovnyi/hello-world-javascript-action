49022cfd0661360ee11c3115a125fa8643126c35
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2022-06-04 23:51:59 +0300

    typo fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   console.log(`Hello ${nameToGreet}`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  const payload = JSON.stringify(guthub.context.payload, undefined, 2);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
